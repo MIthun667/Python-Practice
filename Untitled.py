ad0aa54b383d9e0cb95aab8e001b83422c132342
Md.Mithun Hossain <80522075+MIthun667@users.noreply.github.com>
2023-01-22 17:57:28 +0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[1]:
+
+
+get_ipython().system('pip install numpy')
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+my_list1 = [1, 2, 3, 4]
+array = np.array(my_list)
+
+
+# In[7]:
+
+
+my_list
+
+
+# In[8]:
+
+
+my_list2= [11, 12, 13, 14]
+my_lists = (my_list1, my_list2)
+
+
+# In[9]:
+
+
+array2 = np.array(my_lists)
+
+
+# In[10]:
+
+
+array2
+
+
+# In[ ]:
+
+
+
+
